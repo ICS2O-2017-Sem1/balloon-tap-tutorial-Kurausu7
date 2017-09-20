133a3e169708cbb351f83f5a791101076db2f85c
Kurausu7 <s292714@ocsb.local>
2017-09-20 09:57:30 -0400

    mario

--- main.lua
+++ main.lua
@@ -11,13 +11,18 @@ local platform = display.newImageRect( "platform.png", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
 
-local balloon = display.newImageRect( "balloon.png", 112, 112 )
-balloon.x = display.contentCenterX
-balloon.y = display.contentCenterY
-balloon.alpha = 0.8
+local Mario = display.newImageRect( "Mario.png", 112, 112 )
+Mario.x = display.contentCenterX
+Mario.y = display.contentCenterY
+Mario.alpha = 0.8
 
 local physics = require( "physics" )
 physics.start( )
-
 physics.addBody(platform, "static" )
-physics.addBody(balloon, "dynamic", { radius=50, bounce=0.5} )
+physics.addBody(Mario, "dynamic", { radius=50, bounce=0.3} )
+
+local function pushMario()
+    Mario:applyLinearImpulse( 0, -0.75, Mario.x, Mario.y)
+end
+
+Mario:addEventListener( "tap", pushMario )
