e0af6c7a275066fd7a026e172d488e353bf5e77a
Kurausu7 <s292714@ocsb.local>
2017-09-21 11:16:27 -0400

    2.0

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 local background = display.newImageRect( "background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
-local platform = display.newImageRect( "platform.png", 300, 50 )
+local platform = display.newImageRect( "ssbS.jpg", 300, 50 )
 platform.x = display.contentCenterX
 platform.y = display.contentHeight-25
 
